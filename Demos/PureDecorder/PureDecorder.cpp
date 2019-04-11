e845d2bf1ebc1898e4538ae3c7274ea32b09e4f7
Iann Dang <iann1978@163.com>
2019-04-11 11:07:14 +0800

    finish the demo PureDecorder

--- Demos/PureDecorder/PureDecorder.cpp
+++ Demos/PureDecorder/PureDecorder.cpp
@@ -1,6 +1,6 @@
 ﻿/**
- * 最简单的SDL2播放视频的例子（SDL2播放RGB/YUV）
- * Simplest Video Play SDL2 (SDL2 play RGB/YUV)
+ * 最简单的基于FFmpeg的视频解码器（纯净版）
+ * Simplest FFmpeg Decoder Pure
  *
  * 雷霄骅 Lei Xiaohua
  * leixiaohua1020@126.com
@@ -8,171 +8,218 @@
  * Communication University of China / Digital TV Technology
  * http://blog.csdn.net/leixiaohua1020
  *
- * 本程序使用SDL2播放RGB/YUV视频像素数据。SDL实际上是对底层绘图
- * API（Direct3D，OpenGL）的封装，使用起来明显简单于直接调用底层
- * API。
  *
- * 函数调用步骤如下:
- *
- * [初始化]
- * SDL_Init(): 初始化SDL。
- * SDL_CreateWindow(): 创建窗口（Window）。
- * SDL_CreateRenderer(): 基于窗口创建渲染器（Render）。
- * SDL_CreateTexture(): 创建纹理（Texture）。
- *
- * [循环渲染数据]
- * SDL_UpdateTexture(): 设置纹理的数据。
- * SDL_RenderCopy(): 纹理复制给渲染器。
- * SDL_RenderPresent(): 显示。
- *
- * This software plays RGB/YUV raw video data using SDL2.
- * SDL is a wrapper of low-level API (Direct3D, OpenGL).
- * Use SDL is much easier than directly call these low-level API.
- *
- * The process is shown as follows:
- *
- * [Init]
- * SDL_Init(): Init SDL.
- * SDL_CreateWindow(): Create a Window.
- * SDL_CreateRenderer(): Create a Render.
- * SDL_CreateTexture(): Create a Texture.
- *
- * [Loop to Render data]
- * SDL_UpdateTexture(): Set Texture's data.
- * SDL_RenderCopy(): Copy Texture to Render.
- * SDL_RenderPresent(): Show.
+ * 本程序实现了视频码流(支持HEVC，H.264，MPEG2等)解码为YUV数据。
+ * 它仅仅使用了libavcodec（而没有使用libavformat）。
+ * 是最简单的FFmpeg视频解码方面的教程。
+ * 通过学习本例子可以了解FFmpeg的解码流程。
+ * This software is a simplest decoder based on FFmpeg.
+ * It decode bitstreams to YUV pixel data.
+ * It just use libavcodec (do not contains libavformat).
+ * Suitable for beginner of FFmpeg.
  */
-
+#pragma warning(disable: 4996)
 #include <stdio.h>
 
+#define __STDC_CONSTANT_MACROS
+
+#ifdef _WIN32
+ //Windows
 extern "C"
 {
-#include "SDL.h"
 #include "libavcodec/avcodec.h"
-#include "libavformat/avformat.h"
-#include "libavfilter/avfilter.h"
 };
-
-#pragma warning (disable:4996)
-
-/**
- * Configuration Information
- */
-char * configurationinfo()
+#else
+ //Linux...
+#ifdef __cplusplus
+extern "C"
 {
-	char info[10000] = { 0 };
-	av_register_all();
-
-	sprintf(info, "%s\n", avcodec_configuration());
-
-	return info;
-}
-
-const int bpp = 12;
-
-int screen_w = 176, screen_h = 144;
-const int pixel_w = 176, pixel_h = 144;
-
-unsigned char buffer[pixel_w*pixel_h*bpp / 8];
+#endif
+#include <libavcodec/avcodec.h>
+#ifdef __cplusplus
+};
+#endif
+#endif
 
 
-//Refresh Event
-#define REFRESH_EVENT  (SDL_USEREVENT + 1)
-
-#define BREAK_EVENT  (SDL_USEREVENT + 2)
-
-int thread_exit = 0;
-
-int refresh_video(void *opaque) {
-	thread_exit = 0;
-	while (!thread_exit) {
-		SDL_Event event;
-		event.type = REFRESH_EVENT;
-		SDL_PushEvent(&event);
-		SDL_Delay(40);
-	}
-	thread_exit = 0;
-	//Break
-	SDL_Event event;
-	event.type = BREAK_EVENT;
-	SDL_PushEvent(&event);
-
-	return 0;
-}
+//test different codec
+#define TEST_H264  1
+#define TEST_HEVC  0
 
 int main(int argc, char* argv[])
 {
-	printf("\n<<Configuration>>\n%s", configurationinfo());
+	AVCodec *pCodec;
+	AVCodecContext *pCodecCtx = NULL;
+	AVCodecParserContext *pCodecParserCtx = NULL;
 
-	if (SDL_Init(SDL_INIT_VIDEO)) {
-		printf("Could not initialize SDL - %s\n", SDL_GetError());
+	FILE *fp_in;
+	FILE *fp_out;
+	AVFrame	*pFrame;
+
+	const int in_buffer_size = 4096;
+	uint8_t in_buffer[in_buffer_size + AV_INPUT_BUFFER_PADDING_SIZE] = { 0 };
+	uint8_t *cur_ptr;
+	int cur_size;
+	AVPacket packet;
+	int ret, got_picture;
+	int y_size;
+
+
+#if TEST_HEVC
+	enum AVCodecID codec_id = AV_CODEC_ID_HEVC;
+	char filepath_in[] = "bigbuckbunny_480x272.hevc";
+#elif TEST_H264
+	AVCodecID codec_id = AV_CODEC_ID_H264;
+	char filepath_in[] = "bigbuckbunny_480x272.h264";
+#else
+	AVCodecID codec_id = AV_CODEC_ID_MPEG2VIDEO;
+	char filepath_in[] = "bigbuckbunny_480x272.m2v";
+#endif
+
+	char filepath_out[] = "bigbuckbunny_480x272.yuv";
+	int first_time = 1;
+
+
+	//av_log_set_level(AV_LOG_DEBUG);
+
+	avcodec_register_all();
+
+	pCodec = avcodec_find_decoder(codec_id);
+	if (!pCodec) {
+		printf("Codec not found\n");
+		return -1;
+	}
+	pCodecCtx = avcodec_alloc_context3(pCodec);
+	if (!pCodecCtx) {
+		printf("Could not allocate video codec context\n");
 		return -1;
 	}
 
-	SDL_Window *screen;
-	//SDL 2.0 Support for multiple windows
-	screen = SDL_CreateWindow("Simplest Video Play SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		screen_w, screen_h, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-	if (!screen) {
-		printf("SDL: could not create window - exiting:%s\n", SDL_GetError());
-		return -1;
-	}
-	SDL_Renderer* sdlRenderer = SDL_CreateRenderer(screen, -1, 0);
-
-	Uint32 pixformat = 0;
-
-	//IYUV: Y + U + V  (3 planes)
-	//YV12: Y + V + U  (3 planes)
-	pixformat = SDL_PIXELFORMAT_IYUV;
-
-	SDL_Texture* sdlTexture = SDL_CreateTexture(sdlRenderer, pixformat, SDL_TEXTUREACCESS_STREAMING, pixel_w, pixel_h);
-
-	FILE *fp = NULL;
-	errno_t err = fopen_s(&fp, "akiyo_qcif.yuv", "rb+");
-
-	if (fp == NULL) {
-		printf("cannot open this file\n");
+	pCodecParserCtx = av_parser_init(codec_id);
+	if (!pCodecParserCtx) {
+		printf("Could not allocate video parser context\n");
 		return -1;
 	}
 
-	SDL_Rect sdlRect;
+	//if(pCodec->capabilities&CODEC_CAP_TRUNCATED)
+	//    pCodecCtx->flags|= CODEC_FLAG_TRUNCATED; 
 
-	SDL_Thread *refresh_thread = SDL_CreateThread(refresh_video, NULL, NULL);
-	SDL_Event event;
+	if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+		printf("Could not open codec\n");
+		return -1;
+	}
+	//Input File
+	fp_in = fopen(filepath_in, "rb");
+	if (!fp_in) {
+		printf("Could not open input stream\n");
+		return -1;
+	}
+	//Output File
+	fp_out = fopen(filepath_out, "wb");
+	if (!fp_out) {
+		printf("Could not open output YUV file\n");
+		return -1;
+	}
+
+	pFrame = av_frame_alloc();
+	av_init_packet(&packet);
+
 	while (1) {
-		//Wait
-		SDL_WaitEvent(&event);
-		if (event.type == REFRESH_EVENT) {
-			if (fread(buffer, 1, pixel_w*pixel_h*bpp / 8, fp) != pixel_w * pixel_h*bpp / 8) {
-				// Loop
-				fseek(fp, 0, SEEK_SET);
-				fread(buffer, 1, pixel_w*pixel_h*bpp / 8, fp);
+
+		cur_size = fread(in_buffer, 1, in_buffer_size, fp_in);
+		if (cur_size == 0)
+			break;
+		cur_ptr = in_buffer;
+
+		while (cur_size > 0) {
+
+			int len = av_parser_parse2(
+				pCodecParserCtx, pCodecCtx,
+				&packet.data, &packet.size,
+				cur_ptr, cur_size,
+				AV_NOPTS_VALUE, AV_NOPTS_VALUE, AV_NOPTS_VALUE);
+
+			cur_ptr += len;
+			cur_size -= len;
+
+			if (packet.size == 0)
+				continue;
+
+			//Some Info from AVCodecParserContext
+			printf("[Packet]Size:%6d\t", packet.size);
+			switch (pCodecParserCtx->pict_type) {
+			case AV_PICTURE_TYPE_I: printf("Type:I\t"); break;
+			case AV_PICTURE_TYPE_P: printf("Type:P\t"); break;
+			case AV_PICTURE_TYPE_B: printf("Type:B\t"); break;
+			default: printf("Type:Other\t"); break;
 			}
+			printf("Number:%4d\n", pCodecParserCtx->output_picture_number);
 
-			SDL_UpdateTexture(sdlTexture, NULL, buffer, pixel_w);
+			ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, &packet);
+			if (ret < 0) {
+				printf("Decode Error.\n");
+				return ret;
+			}
+			if (got_picture) {
+				if (first_time) {
+					printf("\nCodec Full Name:%s\n", pCodecCtx->codec->long_name);
+					printf("width:%d\nheight:%d\n\n", pCodecCtx->width, pCodecCtx->height);
+					first_time = 0;
+				}
+				//Y, U, V
+				for (int i = 0; i < pFrame->height; i++) {
+					fwrite(pFrame->data[0] + pFrame->linesize[0] * i, 1, pFrame->width, fp_out);
+				}
+				for (int i = 0; i < pFrame->height / 2; i++) {
+					fwrite(pFrame->data[1] + pFrame->linesize[1] * i, 1, pFrame->width / 2, fp_out);
+				}
+				for (int i = 0; i < pFrame->height / 2; i++) {
+					fwrite(pFrame->data[2] + pFrame->linesize[2] * i, 1, pFrame->width / 2, fp_out);
+				}
 
-			//FIX: If window is resize
-			sdlRect.x = 0;
-			sdlRect.y = 0;
-			sdlRect.w = screen_w;
-			sdlRect.h = screen_h;
-
-			SDL_RenderClear(sdlRenderer);
-			SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect);
-			SDL_RenderPresent(sdlRenderer);
-
+				printf("Succeed to decode 1 frame!\n");
+			}
 		}
-		else if (event.type == SDL_WINDOWEVENT) {
-			//If Resize
-			SDL_GetWindowSize(screen, &screen_w, &screen_h);
+
+	}
+
+	//Flush Decoder
+	packet.data = NULL;
+	packet.size = 0;
+	while (1) {
+		ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, &packet);
+		if (ret < 0) {
+			printf("Decode Error.\n");
+			return ret;
 		}
-		else if (event.type == SDL_QUIT) {
-			thread_exit = 1;
-		}
-		else if (event.type == BREAK_EVENT) {
+		if (!got_picture) {
 			break;
 		}
+		else {
+			//Y, U, V
+			for (int i = 0; i < pFrame->height; i++) {
+				fwrite(pFrame->data[0] + pFrame->linesize[0] * i, 1, pFrame->width, fp_out);
+			}
+			for (int i = 0; i < pFrame->height / 2; i++) {
+				fwrite(pFrame->data[1] + pFrame->linesize[1] * i, 1, pFrame->width / 2, fp_out);
+			}
+			for (int i = 0; i < pFrame->height / 2; i++) {
+				fwrite(pFrame->data[2] + pFrame->linesize[2] * i, 1, pFrame->width / 2, fp_out);
+			}
+
+			printf("Flush Decoder: Succeed to decode 1 frame!\n");
+		}
 	}
-	SDL_Quit();
+
+	fclose(fp_in);
+	fclose(fp_out);
+
+
+	av_parser_close(pCodecParserCtx);
+
+	av_frame_free(&pFrame);
+	avcodec_close(pCodecCtx);
+	av_free(pCodecCtx);
+
 	return 0;
 }
