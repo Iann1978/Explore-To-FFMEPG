9a94a4dcc0ea10aaa4f9953e38048bf0a2882337
Iann Dang <iann1978@163.com>
2019-04-18 09:19:58 +0800

    + add the ablity of event handle.

--- Demos/SimplePlayer/main.cpp
+++ Demos/SimplePlayer/main.cpp
@@ -7,8 +7,27 @@ extern "C"
 };
 
 
-//Output YUV420P data as a file 
-#define OUTPUT_YUV420P 0
+int thread_exit = 0;
+#define REFRESH_EVENT  (SDL_USEREVENT + 1)
+
+#define BREAK_EVENT  (SDL_USEREVENT + 2)
+int refresh_video(void *opaque) {
+	thread_exit = 0;
+	while (!thread_exit) {
+		SDL_Event event;
+		event.type = REFRESH_EVENT;
+		SDL_PushEvent(&event);
+		SDL_Delay(40);
+	}
+	thread_exit = 0;
+	//Break
+	SDL_Event event;
+	event.type = BREAK_EVENT;
+	SDL_PushEvent(&event);
+
+	return 0;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -54,27 +73,51 @@ int main(int argc, char* argv[])
 	sdlRect.w = screen_w;
 	sdlRect.h = screen_h;
 
-	AVFrame *frame = nullptr;
-	//SDL End----------------------
-	while (frame = player.GetOneFrame()) {
 
-		SDL_UpdateYUVTexture(sdlTexture, &sdlRect,
-			frame->data[0], frame->linesize[0],
-			frame->data[1], frame->linesize[1],
-			frame->data[2], frame->linesize[2]);
+	SDL_Thread *refresh_thread = SDL_CreateThread(refresh_video, NULL, NULL);
+	SDL_Event event;
+	while (1) {
+		//Wait
+		SDL_WaitEvent(&event);
+		if (event.type == REFRESH_EVENT) {
+			AVFrame *frame = player.GetOneFrame();
+			SDL_UpdateYUVTexture(sdlTexture, &sdlRect,
+				frame->data[0], frame->linesize[0],
+				frame->data[1], frame->linesize[1],
+				frame->data[2], frame->linesize[2]);
 
-		player.FreeOneFrame(frame);
-		frame = nullptr;
+			player.FreeOneFrame(frame);
+			
 
+			SDL_RenderClear(sdlRenderer);
+			SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect);
+			SDL_RenderPresent(sdlRenderer);
 
-		SDL_RenderClear(sdlRenderer);
-		SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect);
-		SDL_RenderPresent(sdlRenderer);
-		//SDL End-----------------------
-		//Delay 40ms
-		SDL_Delay(40);
+		}
+		else if (event.type == SDL_WINDOWEVENT) {
+			//If Resize
+			SDL_GetWindowSize(screen, &screen_w, &screen_h);
+		}
+		else if (event.type == SDL_KEYDOWN) {
+			if (event.key.keysym.sym == SDLK_LEFT)
+			{
+				int a = 0;
+			}
+			if (event.key.keysym.sym == SDLK_RIGHT)
+			{
+				int b = 0;
 
+		}
+		else if (event.type == SDL_QUIT) {
+			thread_exit = 1;
+		}
+		else if (event.type == BREAK_EVENT) {
+			break;
+		}
 	}
+	SDL_Quit();
+
+
 
 	return 0;
 }
