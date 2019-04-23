6f4669979923f65950f0dc2cedf9db44c9179e10
Iann Dang <iann1978@163.com>
2019-04-23 13:59:02 +0800

    + support opengl texture.

--- Demos/LibPlayer/dllmain.cpp
+++ Demos/LibPlayer/dllmain.cpp
@@ -3,6 +3,10 @@
 #include <gl/gl.h>
 #include "gl/glext.h"
 #include "gl/wglext.h"
+#include "texture.hpp"
+
+
+
 
 extern "C" int __declspec(dllexport)  GetDllVersion()
 {
@@ -14,8 +18,6 @@ extern "C" int __declspec(dllexport) DrawTriange();
 extern "C" int __declspec(dllexport) CreateGL(HWND wnd)
 {
 	HDC DC = GetDC(wnd);
-
-
 
 	PIXELFORMATDESCRIPTOR fakePFD;
 	ZeroMemory(&fakePFD, sizeof(fakePFD));
@@ -50,9 +52,37 @@ extern "C" int __declspec(dllexport) CreateGL(HWND wnd)
 		return 4;
 	}
 
+	GLuint texture = loadBMP_custom("d:/a.bmp");
+	glEnable(GL_TEXTURE);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
 	glClearColor(1.0f, 0.586f, 0.949f, 1.0f);	// rgb(33,150,243)
 	glClear(GL_COLOR_BUFFER_BIT);
-	DrawTriange();
+	glColor3f(0, 0, 0);
+	
+
+
+	glBegin(GL_TRIANGLES);
+
+	glTexCoord2f(0, 0);
+	glVertex3d(0, 0, 0);
+
+	glTexCoord2f(1, 0);
+	glVertex3d(1, 0, 0);
+
+	glTexCoord2f(0, 1);
+	glVertex3d(0, 1, 0);
+
+	glEnd();
+
+
+
+	//DrawTriange();
 	SwapBuffers(DC);
 	return 0;
 
